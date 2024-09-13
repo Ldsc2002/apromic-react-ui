179639737432e6423885618ea94dc4742e76a6d2
Ldsc2002 <luissantoscuellar@gmail.com>
2024-09-13 16:00:58 -0600

    Added proptypes example

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import classes from '../../style/Button.module.css'
 
-function Button([link]) {
+function Button({ link }) {
     return (
         <a className={classes.link} href={link}>
             This is a button
         </a>
     )
+}
+
+Button.propTypes = {
+    link: PropTypes.string.isRequired,
 }
 
 export default Button
